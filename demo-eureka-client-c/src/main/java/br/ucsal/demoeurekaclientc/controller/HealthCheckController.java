24a699cb62044eb2705ff57e1133a88ed169b127
Kauã Vilas Boas <kauacaldeira@hotmail.com>
2024-04-01 23:57:13 -0300

    feat: new endpoint to get random number on Client C

--- demo-eureka-client-c/src/main/java/br/ucsal/demoeurekaclientc/controller/HealthCheckController.java
+++ demo-eureka-client-c/src/main/java/br/ucsal/demoeurekaclientc/controller/HealthCheckController.java
@@ -1,4 +1,33 @@
 package br.ucsal.demoeurekaclientc.controller;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+@RestController
 public class HealthCheckController {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    private static Random random = new Random();
+
+    @GetMapping("/random-number")
+    public int getRandomNumber() {
+
+        int number = random.nextInt(1,100);
+        return number;
+
+    }
+
 }
